2edcd7ec72d0f2cb942ffbe544cbf60938c51dc0
Jason Wood <jwood@startmail.com>
2024-05-03 14:04:17 +1000

    Added basic arrays to binary format.

---
+++ Engine/src/engine/asset/binary/Array.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Struct.h"
+
+namespace se::asset::binary
+{
+    constexpr uint32_t s_ArrayHeaderSize = sizeof(uint32_t) * 2; // length, structindex
+
+    class Array
+    {
+    public:
+        uint32_t GetCount();
+        Struct GetStruct();
+        uint32_t GetStructIndex();
+        Object Get(uint32_t i);
+    private:
+        Array(uint32_t offset, Database* database);
+        uint32_t GetOffsetOf(uint32_t i);
+        char* GetData();
+
+        uint32_t m_Offset;
+        Database* m_DB;
+        uint32_t m_ElementSize; //cache
+
+        friend class Database;
+    };
+}
