75be229c0c2772093745638ffaa7e2cd10249654
Jason Wood <jwood@startmail.com>
2024-06-03 11:23:10 +1000

    cleanup some debug code

--- Engine/src/engine/ecs/components/MeshComponent.h
+++ Engine/src/engine/ecs/components/MeshComponent.h
@@ -16,14 +16,6 @@ namespace se::ecs::components
         DECLARE_SPARK_COMPONENT(MeshComponent)
 
         MeshComponent();
-        MeshComponent(const MeshComponent& other) : material(other.material), vertBuffer(other.vertBuffer)
-        {
-            int lol =1;
-        }
-        ~MeshComponent()
-        {
-            int lol = 1;
-        }
 
         std::shared_ptr<se::render::Material> material = {};
         std::shared_ptr<se::render::VertexBuffer> vertBuffer = {};
