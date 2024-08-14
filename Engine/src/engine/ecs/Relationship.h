583c316a161d8469a62364b91a5decf4fd6a6918
Jason Wood <jwood@startmail.com>
2024-08-14 20:25:40 +1000

    relationships can now be to components

--- Engine/src/engine/ecs/Relationship.h
+++ Engine/src/engine/ecs/Relationship.h
@@ -17,16 +17,24 @@ namespace se::ecs
         Id GetId() const;
         void SetId(Id id);
     private:
-        Relationship(Id entity);
         Id m_Id;
     };
 
     template <typename T>
-    Relationship CreateRelationship(Id entity)
+    Relationship CreateEntityRelationship(Id entity)
     {
         static_assert(std::is_base_of_v<RelationshipComponent, T>, "Relationship components must inherit from RelationshipComponent.");
         Relationship relationship;
         relationship.SetId(bits::PackUtil::Pack64(bits::PackUtil::UnpackA64(T::GetComponentId()), bits::PackUtil::UnpackA64(entity)));
+        return relationship;
+    }
+
+    template <typename T, typename Y>
+    Relationship CreateComponentRelationship()
+    {
+        static_assert(std::is_base_of_v<RelationshipComponent, T>, "Relationship components must inherit from RelationshipComponent.");
+        Relationship relationship;
+        relationship.SetId(bits::PackUtil::Pack64(bits::PackUtil::UnpackA64(T::GetComponentId()), bits::PackUtil::UnpackA64(Y::GetComponentId())));
         return relationship;
     }
 }
