91397bc0639e79907b8f03e5dfe52b5763f2ad40
Jason Wood <jwood@startmail.com>
2025-04-30 10:11:38 +1000

    WIP Asset browser window.

--- Editor/src/editor/ui/AssetBrowserWindow.cpp
+++ Editor/src/editor/ui/AssetBrowserWindow.cpp
@@ -1,4 +1,3 @@
-#include <engine/ui/components/TextComponent.h>
 #include "AssetBrowserWindow.h"
 #include "engine/ui/components/GridBoxComponent.h"
 #include "engine/ui/components/ImageComponent.h"
@@ -7,10 +6,14 @@
 #include "engine/ui/components/WidgetComponent.h"
 #include "engine/ui/components/TitleBarComponent.h"
 #include "engine/ui/components/RectTransformComponent.h"
+#include "engine/ui/components/TextComponent.h"
+#include "engine/ui/components/VerticalBoxComponent.h"
+#include "engine/ui/components/ButtonComponent.h"
 #include "engine/ui/util/WindowUtil.h"
 #include "engine/render/FrameBuffer.h"
 #include "engine/render/MaterialInstance.h"
 #include "engine/asset/AssetManager.h"
+#include "engine/ui/util/VerticalBoxUtil.h"
 
 namespace se::editor::ui
 {
@@ -18,6 +21,7 @@ namespace se::editor::ui
     {
         auto app = Application::Get();
         auto world = app->GetWorld();
+        auto assetManager = se::asset::AssetManager::Get();
 
         se::ui::components::RectTransformComponent *windowTransform;
         se::ui::components::WindowComponent *windowComp;
@@ -37,66 +41,42 @@ namespace se::editor::ui
         windowTransform->minY = 390;
         windowTransform->maxY = 700;
 
-        ecs::Id gridBoxEntity = world->CreateEntity("Grid Box", true);
-        auto* gridRect = world->AddComponent<se::ui::components::RectTransformComponent>(gridBoxEntity);
+        ecs::Id verticalBoxEntity = world->CreateEntity("AssetBrowser VerticalBox", true);
+        auto* verticalBoxRect = world->AddComponent<se::ui::components::RectTransformComponent>(verticalBoxEntity);
+        verticalBoxRect->overridesChildSizes = true;
+        verticalBoxRect->anchors = { .left = 0.f, .right = 1.f, .top = 0.f, .bottom = 1.f };
+        verticalBoxRect->minX = 5;
+        verticalBoxRect->minY = 5;
+        verticalBoxRect->maxX = 5;
+        verticalBoxRect->maxY = 5;
+        auto* verticalBox = world->AddComponent<se::ui::components::VerticalBoxComponent>(verticalBoxEntity);
+        verticalBox->spacing = 5;
+        world->AddComponent<se::ui::components::WidgetComponent>(verticalBoxEntity);
+        world->AddChild(contentArea, verticalBoxEntity);
+
+        auto arial = assetManager->GetAsset<asset::Font>("/builtin_assets/fonts/Arial.sass");
+        auto textEntity = world->CreateEntity("Text", true);
+        auto text = world->AddComponent<se::ui::components::TextComponent>(textEntity);
+        text->font = arial;
+        text->fontSize = 21;
+        text->text = "/assets";
+        world->AddComponent<se::ui::components::RectTransformComponent>(textEntity);
+        world->AddComponent<se::ui::components::WidgetComponent>(textEntity);
+        se::ui::util::AddVerticalBoxChild(verticalBoxEntity, verticalBox, textEntity);
+
+        m_GridBoxEntity = world->CreateEntity("Grid Box", true);
+        auto* gridRect = world->AddComponent<se::ui::components::RectTransformComponent>(m_GridBoxEntity);
         gridRect->overridesChildSizes = true;
         gridRect->anchors = { .left = 0.f, .right = 1.f, .top = 0.f, .bottom = 1.f };
-        gridRect->minX = 5;
-        gridRect->minY = 5;
-        gridRect->maxX = 5;
-        gridRect->maxY = 5;
-        auto* gridBox = world->AddComponent<se::ui::components::GridBoxComponent>(gridBoxEntity);
+        auto* gridBox = world->AddComponent<se::ui::components::GridBoxComponent>(m_GridBoxEntity);
         gridBox->spacing = 5;
-        world->AddComponent<se::ui::components::WidgetComponent>(gridBoxEntity);
-        world->AddChild(contentArea, gridBoxEntity);
+        world->AddComponent<se::ui::components::WidgetComponent>(m_GridBoxEntity);
+        se::ui::util::AddVerticalBoxChild(verticalBoxEntity, verticalBox, m_GridBoxEntity);
 
-        auto assetManager = se::asset::AssetManager::Get();
-        m_TestTexture = assetManager->GetAsset<asset::Texture>("/builtin_assets/textures/default_file.sass");
-        auto arial = assetManager->GetAsset<asset::Font>("/builtin_assets/fonts/Arial.sass");
-        for (size_t i = 0; i < 10; ++i)
-        {
-            ecs::Id fileEntity = world->CreateEntity("File", true);
-            auto rect = world->AddComponent<se::ui::components::RectTransformComponent>(fileEntity);
-            rect->maxX = 100;
-            rect->maxY = 100;
+        m_FileTexture = assetManager->GetAsset<asset::Texture>("/builtin_assets/textures/default_file.sass");
+        m_FolderTexture = assetManager->GetAsset<asset::Texture>("/builtin_assets/textures/default_folder.sass");
 
-            ecs::Id imageEntity = world->CreateEntity("Image", true);
-            auto imageRect = world->AddComponent<se::ui::components::RectTransformComponent>(imageEntity);
-            imageRect->anchors = { .left = 0.f, .right = 1.f, .top = 0.f, .bottom = 1.f };
-            imageRect->maxY = 20;
-            imageRect->minAspectRatio = 1.f;
-            imageRect->maxAspectRatio = 1.f;
-            auto image = world->AddComponent<se::ui::components::ImageComponent>(imageEntity);
-            static std::shared_ptr<render::Material> material = nullptr;
-            if (!material)
-            {
-                auto vert = assetManager->GetAsset<asset::Shader>("/builtin_assets/shaders/ui.sass");
-                auto frag = assetManager->GetAsset<asset::Shader>("/builtin_assets/shaders/alpha_texture.sass");
-
-                material = render::Material::CreateMaterial({vert}, {frag});
-                auto rs = render::RenderState();
-                rs.srcBlend = render::BlendMode::SrcAlpha;
-                rs.dstBlend = render::BlendMode::OneMinusSrcAlpha;
-                material->SetRenderState(rs);
-            }
-            image->materialInstance = se::render::MaterialInstance::CreateMaterialInstance(material);
-            image->materialInstance->SetUniform("Texture", asset::shader::ast::AstType::Sampler2D, 1, &m_TestTexture);
-            world->AddChild(fileEntity, imageEntity);
-
-            auto textEntity = world->CreateEntity("Text", true);
-            auto text = world->AddComponent<se::ui::components::TextComponent>(textEntity);
-            text->font = arial;
-            text->fontSize = 16;
-            text->text = "File";
-            text->justification = se::ui::text::Alignment::Center;
-            auto textRect = world->AddComponent<se::ui::components::RectTransformComponent>(textEntity);
-            textRect->anchors = { .left = 0.f, .right = 1.f, .top = 1.f, .bottom = 1.f };
-            textRect->minY = 20;
-            world->AddComponent<se::ui::components::WidgetComponent>(textEntity);
-            world->AddChild(fileEntity, textEntity);
-
-            world->AddChild(gridBoxEntity, fileEntity);
-        }
+        SetActiveFolder("/assets");
 
         gridBox->dirty = true;
     }
@@ -104,5 +84,109 @@ namespace se::editor::ui
     void AssetBrowserWindow::DestroyUI()
     {
 
+    }
+
+    void AssetBrowserWindow::SetActiveFolder(const String &activeFolder)
+    {
+        m_ActiveFolder = activeFolder;
+
+        auto world = Application::Get()->GetWorld();
+        auto assetManager = se::asset::AssetManager::Get();
+        auto arial = assetManager->GetAsset<asset::Font>("/builtin_assets/fonts/Arial.sass");
+
+        for (const auto &child: world->GetChildren(m_GridBoxEntity))
+        {
+            world->DestroyEntity(child);
+        }
+
+        auto& vfs = io::VFS::Get();
+        vfs.ForEachFile(m_ActiveFolder, false, [this, world, assetManager, &arial](const io::VFSFile& file)
+        {
+            auto fileEntity = CreateFileItem(world, assetManager, file, arial);
+            if (fileEntity != ecs::s_InvalidEntity)
+            {
+                world->AddChild(m_GridBoxEntity, fileEntity);
+            }
+        });
+
+        auto gridBox = world->GetComponent<se::ui::components::GridBoxComponent>(m_GridBoxEntity);
+        gridBox->dirty = true;
+    }
+
+    ecs::Id AssetBrowserWindow::CreateFileItem(ecs::World* world,
+                                               asset::AssetManager* assetManager,
+                                               const io::VFSFile& file,
+                                               const std::shared_ptr<asset::Font>& font)
+    {
+        if (file.fileName.Size() == 0)
+        {
+            return ecs::s_InvalidEntity;
+        }
+
+        ecs::Id fileEntity = world->CreateEntity("File", true);
+        auto rect = world->AddComponent<se::ui::components::RectTransformComponent>(fileEntity);
+        rect->maxX = 100;
+        rect->maxY = 100;
+
+        ecs::Id buttonEntity = world->CreateEntity("Button", true);
+        auto buttonRect = world->AddComponent<se::ui::components::RectTransformComponent>(buttonEntity);
+        buttonRect->anchors = { .left = 0.f, .right = 1.f, .top = 0.f, .bottom = 1.f };
+        auto buttonWidget = world->AddComponent<se::ui::components::WidgetComponent>(buttonEntity);
+        buttonWidget->renderingEnabled = false;
+        auto button = world->AddComponent<se::ui::components::ButtonComponent>(buttonEntity);
+        button->onReleased.Subscribe([this, file]()
+        {
+            if (file.isDirectory)
+            {
+                SetActiveFolder(m_ActiveFolder + '/' + file.fileName);
+            }
+        });
+        world->AddChild(fileEntity, buttonEntity);
+
+        ecs::Id imageEntity = world->CreateEntity("Image", true);
+        auto imageRect = world->AddComponent<se::ui::components::RectTransformComponent>(imageEntity);
+        imageRect->anchors = { .left = 0.f, .right = 1.f, .top = 0.f, .bottom = 1.f };
+        imageRect->maxY = 20;
+        imageRect->minAspectRatio = 1.f;
+        imageRect->maxAspectRatio = 1.f;
+        auto image = world->AddComponent<se::ui::components::ImageComponent>(imageEntity);
+        static std::shared_ptr<render::Material> material = nullptr;
+        if (!material)
+        {
+            auto vert = assetManager->GetAsset<asset::Shader>("/builtin_assets/shaders/ui.sass");
+            auto frag = assetManager->GetAsset<asset::Shader>("/builtin_assets/shaders/alpha_texture.sass");
+
+            material = render::Material::CreateMaterial({vert}, {frag});
+            auto rs = render::RenderState();
+            rs.srcBlend = render::BlendMode::SrcAlpha;
+            rs.dstBlend = render::BlendMode::OneMinusSrcAlpha;
+            material->SetRenderState(rs);
+        }
+        image->materialInstance = se::render::MaterialInstance::CreateMaterialInstance(material);
+
+        if (file.isDirectory)
+        {
+            image->materialInstance->SetUniform("Texture", asset::shader::ast::AstType::Sampler2D, 1, &m_FolderTexture);
+        }
+        else
+        {
+            image->materialInstance->SetUniform("Texture", asset::shader::ast::AstType::Sampler2D, 1, &m_FileTexture);
+        }
+
+        world->AddChild(fileEntity, imageEntity);
+
+        auto labelEntity = world->CreateEntity("Text", true);
+        auto labelText = world->AddComponent<se::ui::components::TextComponent>(labelEntity);
+        labelText->font = font;
+        labelText->fontSize = 16;
+        labelText->text = file.fileName;
+        labelText->justification = se::ui::text::Alignment::Center;
+        auto textRect = world->AddComponent<se::ui::components::RectTransformComponent>(labelEntity);
+        textRect->anchors = { .left = 0.f, .right = 1.f, .top = 1.f, .bottom = 1.f };
+        textRect->minY = 20;
+        world->AddComponent<se::ui::components::WidgetComponent>(labelEntity);
+        world->AddChild(fileEntity, labelEntity);
+
+        return fileEntity;
     }
 }
--- Editor/src/editor/ui/AssetBrowserWindow.h
+++ Editor/src/editor/ui/AssetBrowserWindow.h
@@ -4,9 +4,21 @@
 #include "engine/ecs/ecs_fwd.h"
 #include "engine/ui/Rect.h"
 
+namespace se::ecs
+{
+    class World;
+}
+
 namespace se::asset
 {
+    class AssetManager;
     class Texture;
+    class Font;
+}
+
+namespace se::io
+{
+    struct VFSFile;
 }
 
 namespace se::editor::ui
@@ -19,7 +31,16 @@ namespace se::editor::ui
         void DestroyUI() override;
 
     private:
+        void SetActiveFolder(const String& activeFolder);
+        ecs::Id CreateFileItem(ecs::World* world,
+                               asset::AssetManager* assetManager,
+                               const io::VFSFile& name,
+                               const std::shared_ptr<asset::Font>& font);
+
         ecs::Id m_Window;
-        std::shared_ptr<asset::Texture> m_TestTexture;
+        ecs::Id m_GridBoxEntity;
+        String m_ActiveFolder = "/assets";
+        std::shared_ptr<asset::Texture> m_FileTexture = nullptr;
+        std::shared_ptr<asset::Texture> m_FolderTexture = nullptr;
     };
 }
