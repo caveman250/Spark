5e5947e646ca9ee3237962e909b1c2830f71be85
Jason Wood <jwood@startmail.com>
2025-05-17 16:29:39 +1000

    Added a custom editor for static meshes.

---
+++ Editor/src/editor/ui/properties/StaticMeshEditor.cpp
@@ -0,0 +1,43 @@
+#include "StaticMeshEditor.h"
+
+#include <engine/Application.h>
+#include <engine/ui/components/RectTransformComponent.h>
+
+#include "engine/ui/components/TextComponent.h"
+
+namespace se::editor::ui::properties
+{
+    DEFINE_PROPERTY_EDITOR(se::asset::StaticMesh, StaticMeshEditor, StaticMesh);
+
+    DEFINE_SPARK_CLASS_BEGIN(StaticMeshEditor)
+    DEFINE_SPARK_CLASS_END(StaticMeshEditor)
+
+    void StaticMeshEditor::SetValue(void* value, const reflect::Type*)
+    {
+        m_Value = static_cast<asset::StaticMesh*>(value);
+    }
+
+    void StaticMeshEditor::ConstructUI(const String& name, bool constructTitle, const se::ui::Anchors& anchors)
+    {
+        PropertyEditor::ConstructUI(name, constructTitle, anchors);
+
+        auto world = Application::Get()->GetWorld();
+
+        auto textEntity = world->CreateEntity("StaticMeshEditor Text", true);
+        auto* textRect = world->AddComponent<se::ui::components::RectTransformComponent>(textEntity);
+        textRect->anchors = { 0.f, 1.f, 0.f, 0.f };
+        auto text = world->AddComponent<TextComponent>(textEntity);
+        text->text = std::format("Num Vertices: {}\nNum Indices: {}\nNum Normals: {}\nNum UVs: {}",
+            m_Value->vertices.size(),
+            m_Value->indices.size(),
+            m_Value->uvs.size(),
+            m_Value->normals.size());
+        text->font = asset::AssetManager::Get()->GetAsset<asset::Font>("/engine_assets/fonts/Arial.sass");
+        text->fontSize = 14;
+        world->AddChild(m_Content, textEntity);
+    }
+
+    void StaticMeshEditor::Update()
+    {
+    }
+}
---
+++ Editor/src/editor/ui/properties/StaticMeshEditor.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "NumberEditor.h"
+#include "PropertyEditor.h"
+#include "engine/math/math.h"
+
+namespace se::editor::ui::properties
+{
+    class StaticMeshEditor : public PropertyEditor
+    {
+        DECLARE_SPARK_CLASS(StaticMeshEditor)
+    public:
+        void SetValue(void* value, const reflect::Type* type) override;
+        void ConstructUI(const String& name, bool constructTitle, const se::ui::Anchors& anchors) override;
+        PropertyTitleMode::Type GetTitleMode() const override { return PropertyTitleMode::NextLine; }
+        void Update() override;
+
+    private:
+        asset::StaticMesh* m_Value = nullptr;
+    };
+}
