3c96b2f1ada720b2ccfcaf1fdd9446812158eef1
Jason Wood <jwood@startmail.com>
2025-06-09 19:18:42 +1000

    add reflection for widget comonent

--- engine/src/engine/ui/components/WidgetComponent.h
+++ engine/src/engine/ui/components/WidgetComponent.h
@@ -9,10 +9,19 @@ namespace se::ui::components
     {
         SPARK_COMPONENT()
 
+        SPARK_MEMBER(Serialized)
         Visibility visibility = Visibility::Visible;
+
+        SPARK_MEMBER()
         Visibility parentVisibility = Visibility::Visible;
+
+        SPARK_MEMBER(Serialized)
         bool updateEnabled = true;
+
+        SPARK_MEMBER()
         bool parentUpdateEnabled = true;
+
+        SPARK_MEMBER(Serialized)
         bool dirty = false;
     };
 }
